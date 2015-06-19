fdb03671f328b919b19788c9a595fc48cacec857
BigKuLaK <dnadragon@gmx.net>
2015-06-19 14:42:07 +0200

    1.5 fun_auslagerung

--- LocateU_DK/app/src/main/java/com/kulak/locateu_dk/LocateU.java
+++ LocateU_DK/app/src/main/java/com/kulak/locateu_dk/LocateU.java
@@ -1,7 +1,6 @@
 package com.kulak.locateu_dk;
 
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
@@ -14,10 +13,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.os.StrictMode;
-
-/**
- * Created by Warburg on 18.06.2015.
- */
 
 
 
@@ -90,37 +85,36 @@ public class LocateU extends FragmentActivity {
 
         //------------------------------------------------------------------------------------------
 
-        // get curent Position KK
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        double current_longitude = location.getLongitude();
-        double current_latitude = location.getLatitude();
-
-        //------------------------------------------------------------------------------------------
-
         //Routing KK
-        LatLng fromPosition = new LatLng( current_latitude, current_longitude);
-        LatLng toPosition = new LatLng(49.112532, 9.743714);
+
 
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-        md = new GMapV2Direction();
         mMap = ((SupportMapFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
-        mMap.addMarker(new MarkerOptions().position(fromPosition).title("Start"));
-        mMap.addMarker(new MarkerOptions().position(toPosition).title("End"));
 
+        LatLng fromPosition = get_curent_position();
+        LatLng toPosition = new LatLng(49.112532, 9.743714);
         GMapV2Direction.routeberechnen(fromPosition,toPosition,mMap); // routeberechnen in GMapV2Direction KK
 
         //------------------------------------------------------------------------------------------
 
 
     }
-
-
+    // get curent Position KK
+    public LatLng get_curent_position() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double current_longitude = location.getLongitude();
+        double current_latitude = location.getLatitude();
+        LatLng cposi = new LatLng(current_latitude,current_longitude);
+        return cposi;
+        //------------------------------------------------------------------------------------------
     }
+
+}
 
 
 
